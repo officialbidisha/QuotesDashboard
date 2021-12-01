6e4dc4b9fe3ad95d8c28282f124d0ce2d8fdfd3f
Bidisha <officialbidisha1@gmail.com>
2021-12-01 14:25:26 +0530

    Added Prompt for used to not accidentally leave the Quote Form

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,15 @@
 import QuoteForm from '../components/quotes/QuoteForm';
+import { useHistory } from 'react-router-dom';
 const NewQuote = () => {
-
+    const history = useHistory();
     const addQuoteHandler = quoteData => {
         console.log(quoteData);
+        /**
+         * With push you can go back to the back button
+         * With replace we cannot.
+         * Programmatically move it to the quotes page
+         */
+        history.push('/quotes');
     }
     return <QuoteForm onAddQuote={addQuoteHandler}/>
 }
